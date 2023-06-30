5c18c6318050d8bed35fac42737ba1f057e91367
Suzanne Schuber <suzanneschuber@gmail.com>
2023-06-30 12:18:48 -0700

    WIP business logic in js.

--- src/currency-exchanger.js
+++ src/currency-exchanger.js
@@ -1,6 +1,6 @@
 export default class Exchanger {
   static getExchangeRate(currencyCode) {
-    return fetch(`https://v6.exchangerate-api.com/v6/process.env.API_KEY
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}
     /latest/USD
     `)
       .then(function (response) {
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './css/styles.css';
 import Exchanger from './currency-exchanger';
+
+Exchanger.getExchangeRate(currencyCode);
