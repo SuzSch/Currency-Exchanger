5a8e4a813ca703908e752056b2007a6762456e71
Suzanne Schuber <suzanneschuber@gmail.com>
2023-06-30 16:25:05 -0700

    start API logic over, working through lessons again.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import './css/styles.css';
-import CurrencyExcahnger from './currency-exchanger';
 
-function exchangeCurrency(CurrencyCode) {
-  CurrencyExcahnger.exchangeCurrency(CurrencyCode)
-    .then(function (response) {
-      if (response.main) {
-        printElements(response, CurrencyCode);
-      } else {
-        printError(response);
-      }
-    });
+
+function getConversion(currencyCode) {
+  let request = new XMLHttpRequest();
+  const url = `https://v6.exchangerate-api.com/v6/process.env.API_KEY/latest/US
+]`;
+
+  request.addEventListener("loadend", function () {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, currencyCode);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
 }
-console.log(exchangeCurrency(CurrencyCode))
+
+// UI Logic
+function printElements(apiResponse, currencyCode) {
+  document.querySelector('#showResponse').innerText = `The exchage rate in ${currencyCode} is ${apiResponse.conversion_rates}.`;
+}
+
