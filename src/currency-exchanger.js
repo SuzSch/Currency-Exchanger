0c6661778c15357de757e5886662dbec1819eb1e
Suzanne Schuber <suzanneschuber@gmail.com>
2023-07-04 11:51:17 -0700

    rewrite the API call logic.

--- src/currency-exchanger.js
+++ src/currency-exchanger.js
@@ -1,1 +1,28 @@
 import './css/styles.css';
+export default CurrencyConverter;
+
+class CurrencyConverter {
+  static async convert(usdAmount, targetCurrency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/process.env.API_KEY/latest/USD`);
+      const data = await response.json();
+
+      if (data.result === 'error') {
+        throw new Error(data['error-type']);
+      }
+
+      const exchangeRate = data.conversion_rates[targetCurrency];
+      if (!exchangeRate) {
+        throw new Error('Invalid target currency');
+      }
+
+      const convertedAmount = usdAmount * exchangeRate;
+      return convertedAmount;
+    } catch (error) {
+      throw new Error('Failed to fetch exchange rates');
+    }
+  }
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
 import './css/styles.css';
+import CurrencyConverter from './currency-exchanger';
 
-
-function getConversion(currencyCode) {
-  let request = new XMLHttpRequest();
-  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-
-  request.addEventListener("loadend", function () {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, currencyCode);
-    }
-  });
-
-  request.open("GET", url, true);
-  request.send();
-}
-
-// UI Logic
-function printElements(apiResponse, currencyCode, dollarAmount) {
-  const conversionRate = apiResponse.conversion_rates[currencyCode];
-  const convertedAmount = dollarAmount * conversionRate;
-  document.querySelector('#convertedDollarsDiv').innerText = `The converted amount in ${currencyCode} is ${convertedAmount}.`;
-}
 
