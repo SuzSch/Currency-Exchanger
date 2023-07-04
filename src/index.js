2b401264e9586786788351a6e9e94903ea1315d5
Suzanne Schuber <suzanneschuber@gmail.com>
2023-07-04 12:26:30 -0700

    WIP function to covert currency.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './css/styles.css';
 import CurrencyConverter from './currency-exchanger';
 
-
+function convertCurrency() {
+  const dollarAmount = document.getElementById('dollar-amount').value;
+  const currencyCode = document.getElementById('currency-code').value;
+}
