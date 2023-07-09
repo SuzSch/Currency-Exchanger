0778f26c2c3eb5bbf6edfccc7248209d3667e3af
Suzanne Schuber <suzanneschuber@gmail.com>
2023-07-09 14:07:01 -0700

    Update files, WIP invalid currency message.

--- src/currency-exchanger.js
+++ src/currency-exchanger.js
@@ -1,28 +1,24 @@
 import './css/styles.css';
-export default CurrencyConverter;
-
 class CurrencyConverter {
-  static async convert(usdAmount, targetCurrency) {
+  static async convert(usdAmount, rate) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/process.env.API_KEY/latest/USD`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       const data = await response.json();
+
 
       if (data.result === 'error') {
         throw new Error(data['error-type']);
       }
-
-      const exchangeRate = data.conversion_rates[targetCurrency];
+      const exchangeRate = data.conversion_rates[rate];
       if (!exchangeRate) {
-        throw new Error('Invalid target currency');
-      }
+        throw new Error(`Invalid currency code:`);
 
+      }
       const convertedAmount = usdAmount * exchangeRate;
       return convertedAmount;
     } catch (error) {
-      throw new Error('Failed to fetch exchange rates');
+      throw new Error('Failed to get exchange rates');
     }
   }
 }
-
-
-
+export default CurrencyConverter;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import './css/styles.css';
-import CurrencyConverter from './currency-exchanger';
+import CurrencyConverter from './currency-exchanger.js';
 
-function convertCurrency() {
+document.addEventListener('DOMContentLoaded', () => {
   const dollarAmountInput = document.getElementById('dollar-amount');
   const currencyCodeInput = document.getElementById('currency-code');
+  const conversionButton = document.getElementById('convert-btn');
   const convertedDollarsDiv = document.getElementById('convertedDollarsDiv');
 
-  try {
-    const usdAmount = dollarAmountInput.value;
-    if (isNaN(usdAmount)) {
-      throw new Error('Invalid dollar amount');
+  conversionButton.addEventListener('click', async () => {
+    try {
+      const usdAmount = dollarAmountInput.value;
+      const currencyCode = currencyCodeInput.value;
+
+      if (isNaN(usdAmount) || usdAmount <= 0) {
+        throw new Error('Please enter a valid dollar amount');
+      }
+
+      const convertedAmount = await CurrencyConverter.convert(usdAmount, currencyCode);
+      convertedDollarsDiv.textContent = `$${usdAmount} US Dollars is equivalent to ${convertedAmount.toFixed(2)} ${currencyCode}`;
+    } catch (error) {
+      convertedDollarsDiv.textContent = 'Error: ' + error.message;
     }
+  });
 
+  const form = document.querySelector('.form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-
-
-  }}
+    conversionButton.click();
+  });
+});
